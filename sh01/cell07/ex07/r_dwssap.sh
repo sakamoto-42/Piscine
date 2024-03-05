fbcd27617aa9a949d6dd2bbb119ab24414aaf16a
Julien <julien66140@hotmail.fr>
2024-03-05 18:19:49 +0100

    sh01ex07

--- sh01/cell07/ex07/r_dwssap.sh
+++ sh01/cell07/ex07/r_dwssap.sh
@@ -1,1 +1,2 @@
-cat /etc/passwd
+#!/bin/sh
+cat /etc/passwd | sed '/^#/d' | sed -n '2~2p' | awk -F ':' '{ print $1 }' | rev | sort -r | sed -n ''$FT_LINE1','$FT_LINE2'p' | sed -z 's/\n/, /g' | sed 's/, $/./'
