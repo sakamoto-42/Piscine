b54e7a9acd4ee713740521bd133e67c6f9685396
Julien <julien66140@hotmail.fr>
2024-03-13 20:35:09 +0100

    fix c00ex06 & c00ex07

--- c00/ex06/ft_print_comb2.c
+++ c00/ex06/ft_print_comb2.c
@@ -6,13 +6,46 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 21:07:45 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/10 21:10:30 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/13 20:34:20 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_write_number(int n);
+void	ft_write_numbers(int n);
+
 void	ft_print_comb2(void)
 {
-	write(1, "1", 1);
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_write_numbers(i);
+			write(1, " ", 1);
+			ft_write_numbers(j);
+			write(1, "\n", 1);
+			j++;
+		}
+		i++;
+	}
+}
+
+void	ft_write_number(int n)
+{
+	char	c;
+
+	c = n + 48;
+	write(1, &c, 1);
+}
+
+void	ft_write_numbers(int n)
+{
+	ft_write_number(n / 10);
+	ft_write_number(n % 10);
 }
--- c00/ex06/ft_print_comb3.c
+++ c00/ex06/ft_print_comb3.c
@@ -1,35 +1,59 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/10 21:13:14 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/13 20:01:52 by sakamoto-42      ###   ########.fr       */
+/*   Created: 2024/03/10 21:07:45 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2024/03/12 22:22:25 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+void	ft_write_numbers(int a, int b, int c, int d);
 void	ft_write_number(int n);
-void	ft_write_numbers(int n);
 
-void	ft_putnbr(int n)
+void	ft_print_comb2(void)
 {
-	if (n < 0)
+	int	i;
+	int	j;
+	int	k;
+	int	l;
+
+	i = 0;
+	while (i <= 9)
 	{
-		write(1, "-", 1);
-		n = -n;
+		while (j <= 9)
+		{
+			k = i;
+			l = j + 1;
+			while (k <= 9)
+			{
+				while (l <= 9)
+				{
+					ft_write_numbers(i, j, k, l);
+					l++;
+				}
+				l = 0;
+				k++;
+			}
+			k = 0;
+			j++;
+		}
+		j = 0;
+		i++;
 	}
-	if (n <= 9)
-	{
-		ft_write_number(n);
-	}
-	else
-	{
-		ft_write_numbers(n);
-	}
+}
+
+void	ft_write_numbers(int a, int b, int c, int d)
+{
+	ft_write_number(a);
+	ft_write_number(b);
+	write(1, " ", 1);
+	ft_write_number(c);
+	ft_write_number(d);
 	write(1, "\n", 1);
 }
 
@@ -39,11 +63,4 @@ void	ft_write_number(int n)
 
 	c = n + 48;
 	write(1, &c, 1);
-}
-
-void	ft_write_numbers(int n)
-{
-	if (n >= 10)
-		ft_write_numbers(n / 10);
-	ft_write_number(n % 10);
 }
