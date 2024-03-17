5a443cda13f65dd6c7d03c55a8604e3e46d46789
Julien <julien66140@hotmail.fr>
2024-03-17 20:34:43 +0100

    c00ex07

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,31 +6,35 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 21:13:14 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/14 20:37:06 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/17 20:34:05 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_write_number(int n);
 void	ft_write_numbers(int n);
+void	ft_write_number(int n);
 
 void	ft_putnbr(int n)
 {
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
 	if (n < 0)
 	{
-		if (n == -2147483648)
-		{
-			write(1, "-2147483648", 11);
-			return ;
-		}
-		else
-		{
-			write(1, "-", 1);
-			n = -n;
-		}
+		write(1, "-", 1);
+		n = -n;
 	}
 	ft_write_numbers(n);
+}
+
+void	ft_write_numbers(int n)
+{
+	if (n >= 10)
+		ft_write_numbers(n / 10);
+	ft_write_number(n % 10);
 }
 
 void	ft_write_number(int n)
@@ -39,11 +43,4 @@ void	ft_write_number(int n)
 
 	c = n + 48;
 	write(1, &c, 1);
-}
-
-void	ft_write_numbers(int n)
-{
-	if (n >= 10)
-		ft_write_numbers(n / 10);
-	ft_write_number(n % 10);
 }
