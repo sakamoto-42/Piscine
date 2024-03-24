cbbb11869855b00d4e738bb58de93825080b8278
Julien <julien66140@hotmail.fr>
2024-03-24 21:20:24 +0100

    c01ex03ex04

--- c01/ex03/ft_div_mod.c
+++ c01/ex03/ft_div_mod.c
@@ -6,11 +6,12 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 17:32:56 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/24 17:33:35 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/24 20:25:19 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_div_mod(int a, int b, int *div, int *mod)
 {
-
+	*div = a / b;
+	*mod = a % b;
 }
--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -6,11 +6,17 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 17:37:08 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/24 17:38:42 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/24 20:52:21 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
+	int div;
+	int mod;
 
+	div = *a / *b;
+	mod = *a % *b;
+	*a = div;
+	*b = mod;
 }
