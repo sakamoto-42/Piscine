074db5103fb5c5c130936e2bf2cdcbec62bd1b1a
Julien <julien66140@hotmail.fr>
2024-03-26 20:28:32 +0100

    c01ex08

--- c01/ex08/ft_sort_int_tab.c
+++ c01/ex08/ft_sort_int_tab.c
@@ -6,11 +6,39 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 17:46:24 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/24 17:47:15 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/26 20:28:11 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_swap(int *a, int *b);
+
 void	ft_sort_int_tab(int *tab, int size)
 {
+	int	i;
+	int	j;
+	int	min_i;
 
+	i = 0;
+	while (i < size - 1)
+	{
+		min_i = i;
+		j = i + 1;
+		while (j < size)
+		{
+			if (tab[j] < tab[min_i])
+				min_i = j;
+			j++;
+		}
+		ft_swap(&tab[min_i], &tab[i]);
+		i++;
+	}
+}
+
+void	ft_swap(int *a, int *b)
+{
+	int	temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
 }
