51d5a946de7baa766a9e1583af5c83dc0ae9b3ee
Julien <julien66140@hotmail.fr>
2024-03-26 14:27:48 +0100

    c01ex07

--- c01/ex07/ft_rev_int_tab.c
+++ c01/ex07/ft_rev_int_tab.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 17:43:57 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/26 14:06:03 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/26 14:25:26 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@ void	ft_rev_int_tab(int *tab, int size)
 	int	start;
 	int	end;
 	int	temp;
-	
+
 	start = 0;
-	end = size;
+	end = size - 1;
 	while (start < end)
 	{
 		temp = tab[start];
