e359f4c488863d0f71f8ce9fa22d7218b232430e
Julien <julien66140@hotmail.fr>
2024-03-27 16:51:04 +0100

    c02ex00

--- c02/ex00/ft_putstr.c
+++ c02/ex00/ft_putstr.c
@@ -1,30 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/26 20:37:15 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/27 16:50:08 by sakamoto-42      ###   ########.fr       */
+/*   Created: 2024/03/24 17:39:54 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2024/03/25 21:14:30 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcpy(char *dest, char *src)
+#include <unistd.h>
+
+void	ft_putstr(char *str)
 {
-	int	i;
 	int	size;
+	int	i;
 
 	i = 0;
-	while (src[i] != '\0')
+	while (str[i] != '\0')
 		i++;
 	size = i;
-	i = 0;
-	while (i < size)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i + 1] = '\0';
-	return (dest);
+	write(1, str, size);
 }
