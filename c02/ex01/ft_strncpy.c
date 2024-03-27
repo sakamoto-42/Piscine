7405cf123a1f96bb7cfd41ce1e4390d84b3eb971
Julien <julien66140@hotmail.fr>
2024-03-27 20:06:32 +0100

    c02ex01

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,11 +6,20 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 20:40:34 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/26 20:41:33 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/03/27 20:04:25 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
