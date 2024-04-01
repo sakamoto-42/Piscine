2f09974757e6e0953aa76b36755f46a6f1a5bbbc
Julien <julien66140@hotmail.fr>
2024-04-01 20:07:50 +0200

    fix c02

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 20:43:31 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/28 20:58:33 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/01 18:01:07 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ int	ft_str_is_alpha(char *str)
 
 	i = 0;
 	while (str[i] != '\0')
-	{	
-		if (!(str[i] >= 'a' && str[i] <= 'z') && !(str[i] >= 'A' && str[i] <= 'Z'))
-			return (0);
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z'))
+			if (!(str[i] >= 'A' && str[i] <= 'Z'))
+				return (0);
 		i++;
 	}
 	return (1);
