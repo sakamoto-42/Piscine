e17d7f249e2395cde6165a59400a72dde0f0d992
Julien <julien66140@hotmail.fr>
2024-04-08 13:53:27 +0200

    fix c02ex09 norminette ok

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,9 +6,14 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:11:44 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/08 10:00:22 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/08 13:49:05 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+void	ft_handle_char_lowercase(char *c, int *is_first_letter);
+void	ft_handle_char_uppercase(char *c, int *is_first_letter);
+void	ft_handle_char_numeric(int *is_first_letter);
+void	ft_handle_char_non_alphanumeric(int *is_first_letter);
 
 char	*ft_strcapitalize(char *str)
 {
@@ -20,25 +25,41 @@ char	*ft_strcapitalize(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
-			if (is_first_letter)
-			{
-				str[i] -= 32;
-				is_first_letter = 0;
-			}
-		}
+			handle_char_lowercase(&str[i], &is_first_letter);
 		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
-			if (!is_first_letter)
-				str[i] += 32;
-			else
-				is_first_letter = 0;
-		}
+			handle_char_uppercase(&str[i], &is_first_letter);
 		else if (str[i] >= '0' && str[i] <= '9')
-			is_first_letter = 0;
+			handle_char_numeric(&is_first_letter);
 		else
-			is_first_letter = 1;
+			handle_char_non_alphanumeric(&is_first_letter);
 		i++;
 	}
 	return (str);
+}
+
+void	handle_char_lowercase(char *c, int *is_first_letter)
+{
+	if (*is_first_letter)
+	{
+		*c -= 32;
+		*is_first_letter = 0;
+	}
+}
+
+void	handle_char_uppercase(char *c, int *is_first_letter)
+{
+	if (!*is_first_letter)
+		*c += 32;
+	else
+		*is_first_letter = 0;
+}
+
+void	handle_char_numeric(int *is_first_letter)
+{
+	*is_first_letter = 0;
+}
+
+void	handle_char_non_alphanumeric(int *is_first_letter)
+{
+	*is_first_letter = 1;
 }
