9f678373985fae85d9b24a32155c928e87fc9f85
Julien <julien66140@hotmail.fr>
2024-04-08 14:04:10 +0200

    c02ex09

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:11:44 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/08 13:49:05 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/08 14:01:35 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,19 +25,19 @@ char	*ft_strcapitalize(char *str)
 	while (str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			handle_char_lowercase(&str[i], &is_first_letter);
+			ft_handle_char_lowercase(&str[i], &is_first_letter);
 		else if (str[i] >= 'A' && str[i] <= 'Z')
-			handle_char_uppercase(&str[i], &is_first_letter);
+			ft_handle_char_uppercase(&str[i], &is_first_letter);
 		else if (str[i] >= '0' && str[i] <= '9')
-			handle_char_numeric(&is_first_letter);
+			ft_handle_char_numeric(&is_first_letter);
 		else
-			handle_char_non_alphanumeric(&is_first_letter);
+			ft_handle_char_non_alphanumeric(&is_first_letter);
 		i++;
 	}
 	return (str);
 }
 
-void	handle_char_lowercase(char *c, int *is_first_letter)
+void	ft_handle_char_lowercase(char *c, int *is_first_letter)
 {
 	if (*is_first_letter)
 	{
@@ -46,7 +46,7 @@ void	handle_char_lowercase(char *c, int *is_first_letter)
 	}
 }
 
-void	handle_char_uppercase(char *c, int *is_first_letter)
+void	ft_handle_char_uppercase(char *c, int *is_first_letter)
 {
 	if (!*is_first_letter)
 		*c += 32;
@@ -54,12 +54,12 @@ void	handle_char_uppercase(char *c, int *is_first_letter)
 		*is_first_letter = 0;
 }
 
-void	handle_char_numeric(int *is_first_letter)
+void	ft_handle_char_numeric(int *is_first_letter)
 {
 	*is_first_letter = 0;
 }
 
-void	handle_char_non_alphanumeric(int *is_first_letter)
+void	ft_handle_char_non_alphanumeric(int *is_first_letter)
 {
 	*is_first_letter = 1;
 }
