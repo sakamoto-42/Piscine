25c9684221c7e1664e5d16d2dc08ad6b6fff87e1
Julien <julien66140@hotmail.fr>
2024-04-08 17:43:56 +0200

    c02ex10

--- c02/ex10/ft_strlcpy.c
+++ c02/ex10/ft_strlcpy.c
@@ -6,27 +6,25 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:14:14 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/08 17:24:21 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/08 17:43:13 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned int ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	int	i;
+	unsigned int	i;
 	unsigned int	src_size;
 
 	i = 0;
 	src_size = 0;
-
 	while (src[src_size] != '\0')
-	{
-		while (i < size - 1)
-		{
-			dest[i] = src[i];
-			i++;
-			src_size = i;
-		}
 		src_size++;
+	if (size == 0)
+		return (src_size);
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
 	return (src_size);
