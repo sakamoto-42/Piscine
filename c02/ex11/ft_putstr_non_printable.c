21bce05e9b4e12e7cc3627cdba6253d0c11710ec
Julien <julien66140@hotmail.fr>
2024-04-09 17:03:49 +0200

    c02ex11

--- c02/ex11/ft_putstr_non_printable.c
+++ c02/ex11/ft_putstr_non_printable.c
@@ -6,18 +6,19 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/26 21:18:20 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/09 11:17:04 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/09 15:16:56 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_handle_non_printable_char(char c);
-void	ft_print_hex_char(int value);
+void	ft_print_hex_char(unsigned char c);
 
 void	ft_putstr_non_printable(char *str)
 {
 	int	i;
+	unsigned char	hex_char;
 
 	i = 0;
 	while (str[i] != '\0')
@@ -25,28 +26,20 @@ void	ft_putstr_non_printable(char *str)
 		if (str[i] >= 32 && str[i] <= 126)
 			write(1, &str[i], 1);
 		else
-			ft_handle_non_printable_char(str[i]);
+		{
+			write(1, "\\", 1);
+			hex_char = (unsigned char)str[i];
+			ft_print_hex_char(hex_char);
+		}
 		i++;
 	}
 }
 
-void	ft_handle_non_printable_char(char c)
+void	ft_print_hex_char(unsigned char c)
 {
-	int	char_dec_value;
+	char	*hex;
 
-	char_dec_value = (unsigned char) c;
-	write(1, "\\", 1);
-	ft_print_hex_char(char_dec_value / 16);
-	ft_print_hex_char(char_dec_value % 16);
-}
-
-void	ft_print_hex_char(int value)
-{
-	char	hex_char;
-
-	if (value <= 9)
-		hex_char = value + '0';
-	else
-		hex_char = value - 10 + 'a';
-	write(1, &hex_char, 1);
+	hex = "0123456789abcdef";
+	write(1, &hex[c / 16], 1);
+	write(1, &hex[c % 16], 1);
 }
