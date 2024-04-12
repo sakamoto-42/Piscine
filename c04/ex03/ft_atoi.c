d005ba45af1b5d326d22856d3b0d5226e067fb90
Julien <julien66140@hotmail.fr>
2024-04-12 19:57:57 +0200

    testc02ex03

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,11 +6,54 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:36:14 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/10 13:36:34 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/12 19:57:29 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str)
 {
-	return (0);
+	unsigned int	minus_count;
+	unsigned int	i;
+	int				start;
+	int				end;
+	int				count;
+
+	minus_count = 0;
+	start = -1;
+	count = 0;
+	while	(str[i] != '\0')
+	{
+		if ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)	
+			i++;
+		if (str[i] == 43)
+			i++;
+		if (str[i] == 45)
+		{
+			minus_count++;
+			i++;
+		}
+		if (start == -1 && str[i] >= 48 && str[i] <= 57)
+		{
+			start = i;
+			i++;
+			end = start;
+		}
+		while (str[i] >= 48 && str[i] <= 57)
+		{
+			end = i;
+			i++;
+		}
+	}
+	if (start != -1)
+	{
+		while (start < end)
+		{
+			count += str[start] * 10;
+			start++;
+		}
+	}
+	if (minus_count % 2 == 1)
+		return (count);
+	else
+		return (count);
 }
---
+++ c04/ex03/ft_putnbr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/10 21:13:14 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2024/03/19 20:49:36 by sakamoto-42      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_write_numbers(int n);
+void	ft_write_number(int n);
+
+void	ft_putnbr(int n)
+{
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	ft_write_numbers(n);
+}
+
+void	ft_write_numbers(int n)
+{
+	if (n >= 10)
+		ft_write_numbers(n / 10);
+	ft_write_number(n % 10);
+}
+
+void	ft_write_number(int n)
+{
+	char	c;
+
+	c = n + '0';
+	write(1, &c, 1);
+}
