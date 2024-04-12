69fcaa6952e59e9d4efbae51b1a57b263584bc95
Julien <julien66140@hotmail.fr>
2024-04-12 20:06:55 +0200

    c04ex03

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,54 +6,32 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:36:14 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/12 19:57:29 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/12 20:04:01 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str)
 {
-	unsigned int	minus_count;
-	unsigned int	i;
-	int				start;
-	int				end;
-	int				count;
+	int	i;
+	int	sign;
+	int	result;
 
-	minus_count = 0;
-	start = -1;
-	count = 0;
-	while	(str[i] != '\0')
+	i = 0;
+	sign = 1;
+	result = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)	
+			i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)	
-			i++;
-		if (str[i] == 43)
-			i++;
-		if (str[i] == 45)
-		{
-			minus_count++;
-			i++;
-		}
-		if (start == -1 && str[i] >= 48 && str[i] <= 57)
-		{
-			start = i;
-			i++;
-			end = start;
-		}
-		while (str[i] >= 48 && str[i] <= 57)
-		{
-			end = i;
-			i++;
-		}
+		if (str[i] == '-')
+			sign = -1;
+		i++;
 	}
-	if (start != -1)
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		while (start < end)
-		{
-			count += str[start] * 10;
-			start++;
-		}
+		result = result * 10 + (str[i] - '0');
+		i++;
 	}
-	if (minus_count % 2 == 1)
-		return (count);
-	else
-		return (count);
+	result = result * sign;
+	return (result);
 }
