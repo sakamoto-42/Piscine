e2359851e1cda7c7fef6cfd29fab6ab361a4bf82
Julien <julien66140@hotmail.fr>
2024-04-15 18:35:54 +0200

    c04ex04base

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,13 +6,43 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:38:05 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/12 20:29:10 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/15 18:30:42 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
+int		ft_base_len(char *base);
+void	ft_extract_last_nbr(int	n, int base_len, char *base);
+void	ft_convert_nbr_to_base(int n, char *base);
+
 void	ft_putnbr_base(int nbr, char *base)
 {
-	write(1, "\n", 1);
+	int		base_length;
+
+	base_length = ft_base_len(base);
+	ft_extract_last_nbr(nbr, base_length, base);
+}
+
+int		ft_base_len(char *base)
+{
+	int	i;
+
+	while (base[i] != '\0')
+		i++;
+	return (i);
+}
+
+void	ft_extract_last_nbr(int n, int base_len, char *base)
+{
+	if (n >= base_len)
+		ft_extract_last_nbr(n / base_len, base_len, base);
+	ft_convert_nbr_to_base(n % base_len, base);
+}
+
+void	ft_convert_nbr_to_base(int n, char *base)
+{
+	char c;
+	c = base[n];
+	write(1, &c, 1);
 }
