330a716ad29e29ff157883daea0ce161dd013466
Julien <julien66140@hotmail.fr>
2024-04-16 15:23:48 +0200

    c05ex00

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,11 +6,21 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:47:08 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/10 13:47:37 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/16 15:22:57 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
-	return (nb);
+	int i;
+	int	fact;
+
+	i = 1;
+	fact = nb;
+	while (i < nb)
+	{
+		fact = fact * (nb - i);
+		i++;
+	}
+	return (fact);
 }
