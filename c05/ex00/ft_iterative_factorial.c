3001a82fa53b61284cf54776ce3c313ff50ab0e4
Julien <julien66140@hotmail.fr>
2024-04-16 15:24:47 +0200

    c05ex00fixnorm

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,13 +6,13 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:47:08 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/16 15:22:57 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/16 15:24:06 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_iterative_factorial(int nb)
 {
-	int i;
+	int	i;
 	int	fact;
 
 	i = 1;
