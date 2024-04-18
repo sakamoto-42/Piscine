fa666511d7868a5593535cae0bfe1a2c4adbc02b
Julien <julien66140@hotmail.fr>
2024-04-18 18:09:21 +0200

    c05ex04

--- c05/ex04/ft_fibonacci.c
+++ c05/ex04/ft_fibonacci.c
@@ -1,16 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   fibonacci.c                                        :+:      :+:    :+:   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:00:58 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/10 14:01:26 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/18 18:04:53 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+
+
 int	ft_fibonacci(int index)
 {
-	return (0);
+	int	res;
+
+	if (index < 0)
+		return (-1);
+	else if (index == 0)
+		return (0);
+	else if (index == 1)
+		return (1);
+	res = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+	return (res);
 }
