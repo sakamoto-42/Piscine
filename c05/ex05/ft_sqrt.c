b588263436380d25fee7cd4837fe29fa6def6b7a
Julien <julien66140@hotmail.fr>
2024-04-19 13:21:21 +0200

    c05ex05

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -6,35 +6,52 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:04:24 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/19 11:43:43 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/19 13:20:12 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include "ft_putnbr.h"
+#include <stdio.h>
 
 int	ft_slice_nb(int nb);
+int	ft_search_max_square(int nb);
 
 int	ft_sqrt(int nb)
 {
 	ft_slice_nb(nb);
+
 	return (nb);
 }
 
 int	ft_slice_nb(int nb)
 {
 	int	slice;
+	int max_square;
+	int	rest;
 
 	if (nb % 100 == nb)
 	{
-		ft_putnbr(nb);
-		write(1, "\n", 1);
+		max_square = ft_search_max_square(nb);
+		rest = nb - max_square;
 		return (nb);
 	}
-
 	slice = nb % 100;
 	ft_slice_nb(nb / 100);
-	ft_putnbr(slice);
-	write(1, "\n", 1);
+	max_square = ft_search_max_square(nb);
 	return (slice);
+}
+
+int	ft_search_max_square(int nb)
+{
+	int i;
+
+	i = 0;
+	while (i <= 10)
+	{
+		if (i * i > nb)
+			return ((i - 1) * (i - 1));
+		i++;
+	}
+	return (0);
 }
