10b7a95d0ee4d978b5c771fa44c841ada713eda2
Julien <julien66140@hotmail.fr>
2024-04-19 17:53:38 +0200

    c05ex06

--- c05/ex06/ft_is_prime.c
+++ c05/ex06/ft_is_prime.c
@@ -6,11 +6,45 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:05:56 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/10 14:06:17 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/19 17:53:13 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int	ft_sqrt(int nb);
+int	ft_recursive_is_prime(int n, int sqrt, int i);
+
 int	ft_is_prime(int nb)
 {
-	return (0);
+	int	is_prime;
+	int	sqrt;
+
+	sqrt = ft_sqrt(nb);
+	if (nb <= 1)
+		return (0);
+	is_prime = ft_recursive_is_prime(nb, sqrt, 2);
+	return (is_prime);
+}
+
+int	ft_sqrt(int nb)
+{
+	int	i;
+
+	if (nb < 1)
+		return (0);
+	i = 1;
+	while ((i * i) < nb)
+		i++;
+	return (i);
+}
+
+int	ft_recursive_is_prime(int n, int sqrt, int i)
+{
+	int	is_prime;
+
+	if (i > sqrt)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	is_prime = ft_recursive_is_prime(n, sqrt, i + 1);
+	return (is_prime);
 }
