099e8c70f93fc86e52394b586c82785e1d0c8065
Julien <julien66140@hotmail.fr>
2024-04-19 16:42:55 +0200

    c05ex05v1

--- c05/ex05/ft_sqrt.c
+++ c05/ex05/ft_sqrt.c
@@ -6,73 +6,73 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:04:24 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/19 16:20:42 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/19 16:41:26 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_slice_nb(int nb, int *rest, int *square, int *found);
-void	ft_search_first_square(int nb, int *square);
-void	ft_search_square(int n, int dec, int *rest, int *square);
+int		ft_recursive_slice_nb(int nb, int *rest, int *sqrt, int *found);
+void	ft_search_first_sqrt(int nb, int *sqrt);
+void	ft_search_sqrt(int n, int dec, int *rest, int *sqrt);
 
 int	ft_sqrt(int nb)
 {
 	int	perfect;
-	int	square;
+	int	sqrt;
 	int	rest;
 
 	perfect = 0;
-	square = 0;
+	sqrt = 0;
 	rest = 0;
 	if (nb < 100)
 	{
-		ft_search_first_square(nb, &square);
-		if (square * square == nb)
-			return (square);
+		ft_search_first_sqrt(nb, &sqrt);
+		if (sqrt * sqrt == nb)
+			return (sqrt);
 		return (0);
 	}
-	ft_slice_nb(nb, &rest, &square, &perfect);
+	ft_recursive_slice_nb(nb, &rest, &sqrt, &perfect);
 	if (perfect)
-		return (square);
+		return (sqrt);
 	return (0);
 }
 
-int	ft_slice_nb(int nb, int *rest, int *square, int	*perfect)
+int	ft_recursive_slice_nb(int nb, int *rest, int *sqrt, int *perfect)
 {
 	int	slice;
 	int	res;
 
 	if (nb % 100 == nb)
 	{
-		ft_search_first_square(nb, square);
-		*rest = nb - *square * *square;
+		ft_search_first_sqrt(nb, sqrt);
+		*rest = nb - *sqrt * *sqrt;
 		return (nb);
 	}
 	slice = nb % 100;
-	ft_slice_nb(nb / 100, rest, square, perfect);
+	ft_recursive_slice_nb(nb / 100, rest, sqrt, perfect);
 	res = *rest * 100 + slice;
-	ft_search_square(res, *square * 2, rest, square);
+	ft_search_sqrt(res, *sqrt * 2, rest, sqrt);
 	if (*rest == 0)
 		*perfect = 1;
 	return (slice);
 }
 
-void	ft_search_first_square(int nb, int *square)
+void	ft_search_first_sqrt(int nb, int *sqrt)
 {
 	int	i;
 
 	i = 0;
 	while ((i <= 10) && (i * i <= nb))
 		i++;
-	*square = (i - 1);
+	*sqrt = (i - 1);
 }
 
-void	ft_search_square(int n, int dec, int *rest, int *square)
+void	ft_search_sqrt(int n, int dec, int *rest, int *sqrt)
 {
 	int	i;
 
 	i = 9;
 	while (((dec * 10 + i) * i > n) && (i >= 0))
 		i--;
-	*square = *square * 10 + i;
+	*sqrt = *sqrt * 10 + i;
 	*rest = n - ((dec * 10 + i) * i);
 }
