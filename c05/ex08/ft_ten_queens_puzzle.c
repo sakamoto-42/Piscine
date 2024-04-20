b098ec5f8f9252bf7ce959c8cea28b724cebe8f8
Julien <julien66140@hotmail.fr>
2024-04-20 11:27:42 +0200

    c05ex08v0.001

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:09:44 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/20 10:49:49 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/20 11:18:41 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_ten_queens_puzzle(void)
 	int		board[10][10];
 	int		column;
 	int		line;
+	int		column_to_check;
+	int		line_to_check;
 	char	c;
 
 	// init board
@@ -34,11 +36,14 @@ int	ft_ten_queens_puzzle(void)
 		write(1, "\n", 1);
 		line++;
 	}
+	write(1, "\n", 1);
 
 	// check
 	// begin : 0 0
-	line = 0;
+	column_to_check = 0;
+	line_to_check = 0;
 	column = 0;
+	line = 0;
 	// if empty
 	if (board[column][line] == 0)
 	{
@@ -48,18 +53,45 @@ int	ft_ten_queens_puzzle(void)
 		// if line without queen
 		if (column == 9)
 		{
+			column = 0;
+			line = 0;
 			// check column
 			while (board[column][line] == 0 && line <= 9)
 				line++;
 			// if column without queen
 			if (line == 9)
 			{
+				column = 0;
+				line = 0;
 				// check diagonal
 				while (board[column][line] == 0 || (column <= 9 || line <= 9))
+				{
 					column++;
 					line++;
+				}
+				if (column == 9 || line == 9)
+					board[column_to_check][line_to_check] = 1;
 			}
-		}	
+		}
 	}
+
+	// write board
+	line = 0;
+	while (line <= 9)
+	{
+		column = 0;
+		while (column <= 9)
+		{
+			if (board[column][line] == 0)
+				c = 'O';
+			else
+				c = 'X';
+			write(1, &c, 1);
+			column++;
+		}
+		write(1, "\n", 1);
+		line++;
+	}
+
 	return (board[0][0]);
 }
