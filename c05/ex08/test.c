fc0e9fcba305eea8a98f5ef5571377f631cb7f15
Julien <julien66140@hotmail.fr>
2024-04-21 13:50:58 +0200

    c05ex08v0.2

--- c05/ex08/test.c
+++ c05/ex08/test.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:46:26 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/21 13:29:06 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/21 10:22:23 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_init_board(int board[10][10]);
-int		ft_place_queens(int board[10][10], int cell_row);
-int		ft_place_queen(int board[10][10], int cell_row, int cell_col);
-int		ft_can_place_queen(int board[10][10], int row, int col);
+int		ft_place_queen(int board[10][10], int line, int can_place);
+int		ft_check_cell(int board[10][10], int l, int c);
 void	ft_print_board(int board[10][10]);
 
 int	main()
@@ -23,85 +22,73 @@ int	main()
 	int	board[10][10];
 
 	ft_init_board(board);
-	ft_place_queens(board, 0);
+	ft_place_queen(board, 0, 0);
 	ft_print_board(board);
 	return (0);
 }
 
 void	ft_init_board(int board[10][10])
 {
-	int	row;
-	int	col;
+	int	line;
+	int	column;
 
-	row = 0;
-	while (row <= 9)
+	line = 0;
+	while (line <= 9)
 	{
-		col = 0;
-		while (col <= 9)
+		column = 0;
+		while (column <= 9)
 		{
-			board[col][row] = 0;
-			col++;
+			board[column][line] = 0;
+			column++;
 		}
-		row++;
+		line++;
 	}
 }
 
-int	ft_place_queens(int board[10][10], int cell_row)
+int	ft_place_queen(int board[10][10], int line, int can_place)
 {
-	if (cell_row > 9)
-		return (1);
-	if (!(ft_place_queen(board, cell_row, 0)))
+	if (line > 9)
 		return (0);
-	return (ft_place_queens(board, cell_row + 1));
+	can_place = ft_check_cell(board, line, 0);
+	if (can_place)
+	{
+		return (ft_place_queen(board, line + 1, can_place));
+	}
+	return (ft_place_queen(board, line + 1, can_place));
 }
 
-int	ft_place_queen(int board[10][10], int cell_row, int cell_col)
+int	ft_check_cell(int board[10][10], int l, int c)
 {
-	if (cell_col > 9)
-		return (0);
-	if (ft_can_place_queen(board, cell_row, cell_col))
-	{
-		board[cell_row][cell_col] = 1;
-		return (1);
-	}
-	else
-		return (ft_place_queen(board, cell_row, cell_col + 1));
-}
+	int	i;
+	int j;
 
-int	ft_can_place_queen(int board[10][10], int cell_row, int cell_col)
-{
-	int	r;
-	int	c;
-
-	if (cell_col > 9)
-		return (0);
-	r = cell_row;
-	c = cell_col;
-	while (r >= 0)
+	i = 0;
+	while (i < c)
 	{
-		if (board[r][c] == 1)
+		if (board[i][l] == 1)
 			return (0);
-		r--;
+		i++;
 	}
-	r = cell_row;
-	c = cell_col;
-	while (r >= 0 && c >= 0)
+	j = l;
+	i = c;
+	while (i >= 0 && j >= 0)
 	{
-		if (board[r][c] == 1)
+		if (board[c][j] == 1)
 			return (0);
-		r--;
-		c--;
+		i--;
+		j--;
 	}
-	r = cell_row;
-	c = cell_col;
-	while (r >= 0 && c <= 9)
+	i = l;
+	j = c;
+	if (j >= 0 && i <= 9)
 	{
-		if (board[r][c] == 1)
+		if (board[c][j] == 1)
 			return (0);
-		r--;
-		c++;
+		i++;
+		j--;
 	}
-	return (1);
+	board[c][l] = 1;
+	return (ft_check_cell(board, l, c + 1));
 }
 
 void	ft_print_board(int board[10][10])
