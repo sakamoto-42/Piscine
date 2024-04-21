037b25a24e5bfee6af1f01d1ecca7921336164ad
Julien <julien66140@hotmail.fr>
2024-04-21 15:55:31 +0200

    c05ex08

--- c05/ex08/ft_ten_queens_puzzle.c
+++ c05/ex08/ft_ten_queens_puzzle.c
@@ -6,25 +6,26 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 16:46:26 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/21 15:38:41 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/21 15:52:20 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_init_board(int board[10][10]);
-int		ft_place_queens(int board[10][10], int cell_row);
+int		ft_place_queens(int board[10][10], int cell_row, int *ptr_poss_nb);
 int		ft_can_place_queen(int board[10][10], int row, int col);
-void	ft_print_board(int board[10][10]);
 void	ft_print_placements(int board[10][10]);
 
-int	main()
+int	ft_ten_queens_puzzle(void)
 {
 	int	board[10][10];
+	int	poss_nb;
 
+	poss_nb = 0;
 	ft_init_board(board);
-	ft_place_queens(board, 0);
-	return (0);
+	ft_place_queens(board, 0, &poss_nb);
+	return (poss_nb);
 }
 
 void	ft_init_board(int board[10][10])
@@ -45,7 +46,7 @@ void	ft_init_board(int board[10][10])
 	}
 }
 
-int	ft_place_queens(int board[10][10], int cell_row)
+int	ft_place_queens(int board[10][10], int cell_row, int *ptr_poss_nb)
 {
 	int	col;
 
@@ -53,10 +54,7 @@ int	ft_place_queens(int board[10][10], int cell_row)
 	{
 		ft_print_placements(board);
 		write(1, "\n", 1);
-		write(1, "\n", 1);
-		ft_print_board(board);
-		write(1, "\n", 1);
-		write(1, "\n", 1);
+		(*ptr_poss_nb)++;
 		return (0);
 	}
 
@@ -66,7 +64,7 @@ int	ft_place_queens(int board[10][10], int cell_row)
 		if (ft_can_place_queen(board, cell_row, col))
 		{
 			board[cell_row][col] = 1;
-			if (ft_place_queens(board, cell_row + 1))
+			if (ft_place_queens(board, cell_row + 1, ptr_poss_nb))
 				return (1);
 			board[cell_row][col] = 0;
 		}
