5afc7443e7c3a13dc9e77d367331ae31d89e0425
Julien <julien66140@hotmail.fr>
2024-04-21 17:43:29 +0200

    c06

--- c06/ex00/ft_print_program_name.c
+++ c06/ex00/ft_print_program_name.c
@@ -6,8 +6,20 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:17:52 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/10 14:18:10 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/21 17:41:31 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+int	main(int argc, char **argv)
+{
+	char	*str;
+
+	(void) argc;
+	str = argv[0];
+	
+	while (*str != '\0')
+		write(1, str++, 1);
+	return (0);
+}
