15156c3484d942d9782782846e0726d64ff43cae
Julien <julien66140@hotmail.fr>
2024-04-22 08:15:09 +0200

    c06ex02

--- c06/ex02/ft_rev_params.c
+++ c06/ex02/ft_rev_params.c
@@ -6,13 +6,13 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:18:48 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/21 17:57:51 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/22 08:15:02 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_write_arg(char *str);
+void	ft_write_argv(char *str);
 
 int	main(int argc, char **argv)
 {
@@ -21,13 +21,13 @@ int	main(int argc, char **argv)
 	i = argc - 1;
 	while (i >= 1)
 	{
-		ft_write_arg(argv[i]);
+		ft_write_argv(argv[i]);
 		i--;
 	}
 	return (0);
 }
 
-void	ft_write_arg(char *str)
+void	ft_write_argv(char *str)
 {
 	while (*str != '\0')
 		write(1, str++, 1);
