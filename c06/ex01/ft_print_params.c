050289c43207598612380b6c663a5c316a10fe57
Julien <julien66140@hotmail.fr>
2024-04-22 08:14:40 +0200

    c06ex01

--- c06/ex01/ft_print_params.c
+++ c06/ex01/ft_print_params.c
@@ -6,13 +6,13 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:18:30 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/21 17:52:57 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/22 08:14:29 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void	ft_write_arg(char *str);
+void	ft_write_argv(char *str);
 
 int	main(int argc, char **argv)
 {
@@ -21,13 +21,13 @@ int	main(int argc, char **argv)
 	i = 0;
 	while (i < argc)
 	{
-		ft_write_arg(argv[i]);
+		ft_write_argv(argv[i]);
 		i++;
 	}
 	return (0);
 }
 
-void	ft_write_arg(char *str)
+void	ft_write_argv(char *str)
 {
 	while (*str != '\0')
 		write(1, str++, 1);
