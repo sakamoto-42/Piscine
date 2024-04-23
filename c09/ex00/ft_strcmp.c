63bbe54050c67b6df17c3e49dcb34a8e40c6ba9b
Julien <julien66140@hotmail.fr>
2024-04-23 21:58:39 +0200

    add files

---
+++ c09/ex00/ft_strcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 12:44:56 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2024/04/11 19:13:54 by sakamoto-42      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+	unsigned char	s1_char;
+	unsigned char	s2_char;
+	int				cmp;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	if (s1[i] != s2[i])
+	{
+		s1_char = (unsigned char)s1[i];
+		s2_char = (unsigned char)s2[i];
+		cmp = s1_char - s2_char;
+		return (cmp);
+	}
+	return (0);
+}
