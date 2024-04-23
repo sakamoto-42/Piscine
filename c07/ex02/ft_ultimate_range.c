646bed834c34d086ff8652c173efbf0c2fa76ea5
Julien <julien66140@hotmail.fr>
2024-04-23 20:14:19 +0200

    c07ex02

--- c07/ex02/ft_ultimate_range.c
+++ c07/ex02/ft_ultimate_range.c
@@ -6,11 +6,36 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:25:58 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/10 14:26:33 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/04/23 20:11:03 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_ultimate_range(int **range, int mint, int max)
+#include <stdlib.h>
+
+int	ft_ultimate_range(int **range, int min, int max)
 {
-	return (0);
+	int	length;
+	int	*tab;
+	int	i;
+
+	if (min >= max)
+	{
+		range = NULL;
+		return (0);
+	}
+	length = max - min;
+	tab = malloc(sizeof(**range) * length);
+	if (tab)
+	{
+		i = 0;
+		while (min < max)
+		{
+			tab[i] = min;
+			min++;
+			i++;
+		}
+		range = &tab;
+		return (sizeof(range));
+	}
+	return (-1);
 }
