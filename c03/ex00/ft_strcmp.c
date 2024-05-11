b1ea73cc023fa2a73efc20900bf63f9eb7b47cad
Julien <julien66140@hotmail.fr>
2024-05-11 20:02:07 +0200

    c03ex00 fix opti

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -6,22 +6,23 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 12:44:56 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/11 19:13:54 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/11 19:58:33 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	unsigned int	i;
-	int		cmp;
+	unsigned char	uc1;
+	unsigned char	uc2;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-		i++;
-	if (s1[i] != s2[i])
+	while (*s1 && *s2)
 	{
-		cmp = (unsigned char)s1[i] - (unsigned char)s2[i];
-		return (cmp);
+		uc1 = (unsigned char)*s1;
+		uc2 = (unsigned char)*s2;
+		if (uc1 != uc2)
+			return (uc1 - uc2);
+		s1++;
+		s2++;
 	}
 	return (0);
 }
