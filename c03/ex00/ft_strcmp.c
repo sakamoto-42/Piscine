6eedcf25bd52aebdd93f3a5b0b0c088c7c52fd07
Julien Ducharlet <laliguedesprofs.mathssciences@gmail.com>
2024-05-11 09:17:49 +0200

    fix strcmp

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -13,18 +13,14 @@
 int	ft_strcmp(char *s1, char *s2)
 {
 	unsigned int	i;
-	unsigned char	s1_char;
-	unsigned char	s2_char;
-	int				cmp;
+	int		cmp;
 
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
 		i++;
 	if (s1[i] != s2[i])
 	{
-		s1_char = (unsigned char)s1[i];
-		s2_char = (unsigned char)s2[i];
-		cmp = s1_char - s2_char;
+		cmp = (unsigned char)s1[i] - (unsigned char)s2[i];
 		return (cmp);
 	}
 	return (0);
