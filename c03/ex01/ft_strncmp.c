69099b52ec29b8dd21e5281a7969f7c2099bd8d7
Julien <julien66140@hotmail.fr>
2024-05-12 10:26:02 +0200

    c03ex01opti

--- c03/ex01/ft_strncmp.c
+++ c03/ex01/ft_strncmp.c
@@ -6,30 +6,22 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 12:50:48 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/12 09:56:33 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/11 22:21:53 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
 	unsigned int	i;
-	unsigned char	s1_char;
-	unsigned char	s2_char;
-	int				cmp;
 
 	i = 0;
-	while (i < n)
+	while ((i < n)
+		&& (*s1 && *s2)
+		&& ((unsigned char)*s1 == (unsigned char)*s2))
 	{
-		s1_char = (unsigned char)s1[i];
-		s2_char = (unsigned char)s2[i];
-		if (s1_char != s2_char)
-		{
-			cmp = s1_char - s2_char;
-			return (cmp);
-		}
-		if (s1_char == '\0' || s2_char == '\0')
-			return (0);
+		s1++;
+		s2++;
 		i++;
 	}
-	return (0);
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
