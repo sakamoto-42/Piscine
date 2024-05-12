5b7ebc16078ab93045e29e02ac5747c1767be215
Julien <julien66140@hotmail.fr>
2024-05-12 15:43:16 +0200

    better main

--- c03/ex04/main.c
+++ c03/ex04/main.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:01:47 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/11 20:51:53 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/12 15:41:45 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@
 
 int	main(void)
 {
-	char	str[21];
+	char	str[18];
 	char	to_find[7];
 	char	*res;
 
-	ft_strcpy(str, "Hello needle World !");
+	ft_strcpy(str, "I need a needle !");
 	ft_strcpy(to_find, "needle");
 	res = ft_strstr(str, to_find);
 	ft_putstr(res);
