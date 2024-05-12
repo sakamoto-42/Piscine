a21ce33d694c730b1860267a128d205263b1ac88
Julien <julien66140@hotmail.fr>
2024-05-12 17:53:46 +0200

    c03ex05

--- c03/ex05/ft_strlcat.c
+++ c03/ex05/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 21:37:31 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/05/12 17:33:39 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/12 17:53:12 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,29 @@ unsigned int	ft_strlen(char *str);
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int nb)
 {
+	unsigned int	dest_len;
+	unsigned int	src_len;
 	unsigned int	i;
-
+	
+	dest_len = 0;
 	while (*dest != '\0')
-		dest++;
-
-	i = 0;
-	while (*src != '\0')
 	{
-		if (i < nb)
-		{
-			*dest = *src;
-			dest++;
-			src++;
-		}
+		dest++;
+		dest_len++;
+	}
+	src_len = 0;
+	while (src[src_len] != '\0')
+		src_len++;
+	i = 0;
+	while ((*src != '\0') && (dest_len + i < nb))
+	{
+		*dest = *src;
+		dest++;
+		src++;
 		i++;
 	}
 	*dest = '\0';
-	return (i);
+	return (dest_len + src_len);
 }
 
 unsigned int	ft_strlen(char *str)
