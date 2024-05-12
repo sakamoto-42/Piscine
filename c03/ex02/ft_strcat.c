49595049c899657ae3fa4d69f891f2149e39245a
Julien <julien66140@hotmail.fr>
2024-05-12 10:53:01 +0200

    test

--- c03/ex02/ft_strcat.c
+++ c03/ex02/ft_strcat.c
@@ -6,26 +6,20 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 12:54:44 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/11 19:54:35 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/12 10:47:21 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strcat(char *dest, char *src)
 {
-	unsigned int	i;
-	unsigned int	dest_size;
-
-	i = 0;
-	while (dest[i] != '\0')
-		i++;
-	dest_size = i;
-	i = 0;
-	while (src[i] != '\0')
+	while (*dest != '\0')
+		dest++;
+	while (*src != '\0')
 	{
-		dest[dest_size] = src[i];
-		dest_size++;
-		i++;
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	dest[dest_size] = '\0';
+	*dest = '\0';
 	return (dest);
 }
