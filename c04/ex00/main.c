e4f88caae6cf5a89b0536fafbd9afd72b450c9e1
Julien <julien66140@hotmail.fr>
2024-05-20 09:08:00 +0200

    fix main

--- c04/ex00/main.c
+++ c04/ex00/main.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:29:39 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/22 22:18:41 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/20 09:07:09 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@
 
 int	main(void)
 {
-	char			str[14];
-	unsigned int	len;
+	char	str[14];
+	int		len;
 
 	ft_strcpy(str, "Hello World !");
 	len = ft_strlen(str);
