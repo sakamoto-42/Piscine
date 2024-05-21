fdf2e2d91b9ea507f45eda5fdf9ee77d5ad9b0fa
Julien <julien66140@hotmail.fr>
2024-05-21 18:25:49 +0200

    fix for clarity and better subject accordance

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -6,30 +6,34 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:36:14 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/05/21 17:57:53 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/05/21 18:24:14 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_atoi(char *str)
 {
 	int	sign;
+	int	minus_count;
 	int	result;
 
 	sign = 1;
+	minus_count = 0;
 	result = 0;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
 		str++;
 	while (*str == '-' || *str == '+')
 	{
 		if (*str == '-')
-			sign = -sign;
+			minus_count++;
 		str++;
 	}
+	if (minus_count % 2 != 0)
+		sign = -1;
 	while (*str != '\0' && *str >= '0' && *str <= '9')
 	{
 		result = result * 10 + (*str - '0');
 		str++;
 	}
-	result = result * sign;
+	result = sign * result;
 	return (result);
 }
