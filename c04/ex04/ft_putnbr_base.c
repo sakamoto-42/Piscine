eb3fdd021b5f82568458fd97ca8ab664070029e9
julien <julien66140@hotmail.fr>
2024-06-09 17:03:22 +0200

    fix norm and main

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:38:05 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/06/07 14:31:39 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/06/09 16:58:31 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -72,7 +72,7 @@ int	ft_check_base_error(char *base, long int base_len)
 			return (1);
 		ptr1++;
 	}
-		return (0);
+	return (0);
 }
 
 void	ft_extract_last_nbr(long int n, long int base_len, char *base)
