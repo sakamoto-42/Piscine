2db95a90aee01cfad7216bc9b93856b3b2dddd09
julien <julien66140@hotmail.fr>
2024-06-12 08:12:32 +0200

    optic05ex00

--- c05/ex00/ft_iterative_factorial.c
+++ c05/ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:47:08 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/18 17:58:36 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/06/12 08:06:36 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,7 @@ int	ft_iterative_factorial(int nb)
 		return (0);
 	else if (nb <= 1)
 		return (1);
-	fact = nb;
-	nb = nb - 1;
+	fact = 1;
 	while (nb > 1)
 	{
 		fact = fact * nb;
--- c05/ex00/main.c
+++ c05/ex00/main.c
@@ -6,10 +6,11 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 13:48:02 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/17 15:58:25 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/06/12 08:05:59 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include "ft_putnbr.h"
 #include "ft_iterative_factorial.h"
 
@@ -21,5 +22,6 @@ int	main(void)
 	nb = 5;
 	fact = ft_iterative_factorial(nb);
 	ft_putnbr(fact);
+	write(1, "\n", 1);
 	return (0);
 }
