2c65ab1461546213fff8e6322017b102cb1c0ab4
julien <julien66140@hotmail.fr>
2024-06-13 15:01:57 +0200

    c06ex03 fix

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/10 14:19:05 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/04/25 19:53:51 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/06/13 14:59:27 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,7 @@ int	main(int argc, char **argv)
 	while (i < argc)
 	{
 		ft_write_argv(argv[i]);
+		write(1, "\n", 1);
 		i++;
 	}
 	return (0);
@@ -46,10 +47,8 @@ int	ft_compare_argv(char *str1, char *str2)
 {
 	while (*str1 != '\0' && *str2 != '\0')
 	{
-		if (*str1 > *str2)
-			return (1);
-		else if (*str1 < *str2)
-			return (-1);
+		if (*str1 != *str2)
+			return (*str1 - *str2);
 		str1++;
 		str2++;
 	}
@@ -68,8 +67,5 @@ void	ft_swap_argv(char **argv, int i, int j)
 void	ft_write_argv(char *str)
 {
 	while (*str != '\0')
-	{
 		write(1, str++, 1);
-		write(1, "\n", 1);
-	}
 }
