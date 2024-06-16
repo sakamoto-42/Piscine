41736dd8e45c87ab4b5629cc2864f6ab5c48ab23
julien <julien66140@hotmail.fr>
2024-06-16 14:28:17 +0200

    fix c01ex01

--- c01/ex01/main.c
+++ c01/ex01/main.c
@@ -6,34 +6,35 @@
 /*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 21:17:11 by sakamoto-42       #+#    #+#             */
-/*   Updated: 2024/03/30 10:22:28 by sakamoto-42      ###   ########.fr       */
+/*   Updated: 2024/06/16 14:27:02 by sakamoto-42      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 #include "ft_ultimate_ft.h"
 #include "ft_putnbr.h"
 
 int	main(void)
 {
 	int	n;
-	int	*pt1;
-	int	**pt2;
-	int	***pt3;
-	int	****pt4;
-	int	*****pt5;
-	int	******pt6;
-	int	*******pt7;
-	int	********pt8;
+	int	********ptr;
 
-	pt1 = &n;
-	pt2 = &pt1;
-	pt3 = &pt2;
-	pt4 = &pt3;
-	pt5 = &pt4;
-	pt6 = &pt5;
-	pt7 = &pt6;
-	pt8 = &pt7;
-	ft_ultimate_ft(&pt8);
-	ft_putnbr(********pt8);
+	ptr = malloc(sizeof(int *******));
+	*ptr = malloc(sizeof(int *******));
+	**ptr = malloc(sizeof(int ******));
+	***ptr = malloc(sizeof(int *****));
+	****ptr = malloc(sizeof(int ****));
+	*****ptr = malloc(sizeof(int ***));
+	******ptr = malloc(sizeof(int **));
+	*******ptr = &n;
+	ft_ultimate_ft(&ptr);
+	ft_putnbr(********ptr);
+	free(******ptr);
+	free(*****ptr);
+	free(****ptr);
+	free(***ptr);
+	free(**ptr);
+	free(*ptr);
+	free(ptr);
 	return (0);
 }
