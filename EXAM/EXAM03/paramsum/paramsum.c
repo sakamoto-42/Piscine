99d7a504de4024b780b20c119a0744ae77565212
julien <julien66140@hotmail.fr>
2024-07-05 19:59:31 +0200

    paramsum

---
+++ EXAM/EXAM03/paramsum/paramsum.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sakamoto-42 <sakamoto-42@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 18:35:25 by sakamoto-42       #+#    #+#             */
+/*   Updated: 2024/07/05 19:58:38 by sakamoto-42      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_recursive_write_nb(int nb);
+
+int	main(int argc, char **argv)
+{
+	(void) argv;
+	if (argc - 1 >= 0)
+		ft_write_nb(argc - 1);
+	else
+		write(1, "0", 1);
+	return (0);
+}
+
+void	ft_recursive_write_nb(int nb)
+{
+	if (nb >= 10)
+		ft_recursive_write_nb(nb / 10);
+	nb = (nb % 10) + '0';
+	write(1, &nb, 1);
+}
